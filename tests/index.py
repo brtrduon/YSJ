aaec6867133a9ceab52d83bbf106abc7c90e8a34
brtrduon <brianduongt@gmail.com>
2021-03-08 16:00:46 -0800

    added navigation test

--- tests/index.py
+++ tests/index.py
@@ -10,4 +10,26 @@ class Ysj_test():
     self.driver.get(self.baseUrl)
     self.driver.implicitly_wait(5)
 
+  def navigation_test(self):
+    home = self.driver.find_element_by_link_text("Home")
+    home.click()
+    time.sleep(2)
+
+    menu = self.driver.find_element_by_link_text("Menu")
+    menu.click()
+    time.sleep(2)
+
+    location = self.driver.find_element_by_link_text("Location")
+    location.click()
+    time.sleep(2)
+
+    about = self.driver.find_element_by_id("about")
+    about.click()
+    time.sleep(2)
+
+  def close(self):
+    self.driver.close()
+
 ff = Ysj_test()
+ff.navigation_test()
+ff.close()
